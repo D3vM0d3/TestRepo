efa800ab18d4436654900a08db787774788feabe
Lackó <Lack@192.168.1.65>
2019-12-09 22:24:51 +0100

    Megtanítottam a macskát nyávogni.

---
+++ src/githubrepo/Cat.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubrepo;
+
+/**
+ *
+ * @author Lackó
+ */
+public class Cat {
+    
+    public void meow(){
+        System.out.println("Meow");
+    }
+    
+}
