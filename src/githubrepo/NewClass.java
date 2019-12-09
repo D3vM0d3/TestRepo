55be64f15f3003198a13f9e3d83944c484c2a7d2
Attila <Attila@Yusuke>
2019-12-09 20:53:54 +0100

    Initial commit

---
+++ src/githubrepo/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubrepo;
+
+/**
+ *
+ * @author Attila
+ */
+public class NewClass {
+    
+}
